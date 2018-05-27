70d9d62e09c7fe0485416f2e4a0ef32ce6a78e3d
Shahid Shah <shahid@shah.org>
2018-05-26 22:42:37 -0400

    Added initial logging and persistence functionality

--- main.go
+++ main.go
@@ -12,8 +12,107 @@ import (
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/coreos/pkg/flagutil"
+	"github.com/ericaro/frontmatter"
+	"github.com/peterbourgon/diskv"
 	"github.com/shah/content-harvester-utils"
+	"go.uber.org/zap"
 )
+
+// HarvestedResourceStorage is the database for harvested resources
+type HarvestedResourceStorage struct {
+	basePath         string
+	diskv            *diskv.Diskv
+	logger           *zap.Logger
+	contentHarvester *harvester.ContentHarvester
+}
+
+// SaveAllInText all harvested resources into the database
+func (storage *HarvestedResourceStorage) SaveAllInText(text string) {
+	r := storage.contentHarvester.HarvestResources(text)
+	for _, res := range r.Resources {
+		isURLValid, isDestValid := res.IsValid()
+		if !isURLValid {
+			storage.logger.Info("Invalid URL", zap.String("source", text),
+				zap.String("originalURLText", res.OriginalURLText()),
+				zap.String("reason", "Not sure why"),
+			)
+			continue
+		}
+		if !isDestValid {
+			isIgnored, ignoreReason := res.IsIgnored()
+			if isIgnored {
+				storage.logger.Info("Invalid URL Destination", zap.String("source", text),
+					zap.String("originalURLText", res.OriginalURLText()),
+					zap.String("reason", ignoreReason),
+				)
+			} else {
+				storage.logger.Info("Invalid URL Destination", zap.String("source", text),
+					zap.String("originalURLText", res.OriginalURLText()),
+					zap.String("reason", "Unknown reason"),
+				)
+			}
+			continue
+		}
+		finalURL, resolvedURL, cleanedURL := res.GetURLs()
+		isIgnored, ignoreReason := res.IsIgnored()
+		if isIgnored {
+			storage.logger.Info("Ignored", zap.String("source", text),
+				zap.String("originalURLText", res.OriginalURLText()),
+				zap.String("reason", ignoreReason),
+				zap.String("referredBy", resourceToString(res.ReferredByResource())),
+				zap.String("finalURL", urlToString(finalURL)),
+				zap.String("resolvedURL", urlToString(resolvedURL)),
+			)
+			continue
+		}
+
+		keys := harvester.CreateKeys(res)
+		frontMatter := struct {
+			Slug    string `yaml:"slug"`
+			OrigURL string `yaml:"origURL"`
+			Content string `fm:"content" yaml:"-"`
+		}{
+			Slug:    keys.Slug(),
+			OrigURL: res.OriginalURLText(),
+			Content: text,
+		}
+
+		data, err := frontmatter.Marshal(frontMatter)
+		if err != nil {
+			fmt.Printf("err! %s", err.Error())
+		}
+		storage.diskv.Write(keys.Slug(), data)
+
+		storage.logger.Info("Saved", zap.String("source", text),
+			zap.String("originalURLText", res.OriginalURLText()),
+			zap.String("slug", keys.Slug()),
+			zap.String("referredBy", resourceToString(res.ReferredByResource())),
+			zap.String("finalURL", urlToString(finalURL)),
+			zap.String("resolvedURL", urlToString(resolvedURL)),
+			zap.String("cleanedURL", urlToString(cleanedURL)),
+		)
+	}
+}
+
+// NewHarvestedResourceStorage that can persist harvested resources
+func NewHarvestedResourceStorage(contentHarvester *harvester.ContentHarvester, logger *zap.Logger, basePath string) *HarvestedResourceStorage {
+	result := new(HarvestedResourceStorage)
+	result.contentHarvester = contentHarvester
+	result.logger = logger
+	result.basePath = basePath
+
+	// Simplest transform function: put all the data files into the base dir.
+	flatTransform := func(s string) []string { return []string{} }
+
+	// Initialize a new diskv store, rooted at "my-data-dir", with a 1MB cache.
+	result.diskv = diskv.New(diskv.Options{
+		BasePath:     basePath,
+		Transform:    flatTransform,
+		CacheSizeMax: 1024 * 1024,
+	})
+
+	return result
+}
 
 // *** MAJOR TODO ***
 // The Streaming API is being retired in June:
@@ -150,7 +249,7 @@ func main() {
 	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
 	filterTwitterStream := flags.Bool("filter-stream", false, "Search for content in a continuous Twitter filter (until Ctrl+C is pressed)")
 	searchTwitter := flags.Bool("search", false, "Search for content in Twitter and return results")
-	createTestDataFileName := flags.String("create-test-data", fmt.Sprintf("./tmp/test-data-%s.csv", time.Now().Format("2006-01-02-15-04-05")), "Name of CSV file to generate test data from output")
+	storageBasePath := flags.String("storage-base-path", fmt.Sprintf("./tmp/storage-%s", time.Now().Format("2006-01-02-15-04-05")), "Name of the root directory to storage harvested resources in")
 	flags.Var(&twitterQuery, "query", "The items to search in Twitter Filter")
 	flags.Var(&ignoreURLsRegEx, "ignore-urls-reg-ex", "Regular expression indicating which URL patterns to not harvest")
 	flags.Var(&removeParamsFromURLsRegEx, "remove-params-from-urls-reg-ex", "Regular expression indicating which URL query params to 'clean' in harvested URLs")
@@ -177,35 +276,35 @@ func main() {
 		removeParamsFromURLsRegEx = []*regexp.Regexp{regexp.MustCompile(`^utm_`)}
 	}
 
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
+
 	contentHarvester := harvester.MakeContentHarvester(ignoreURLsRegEx, removeParamsFromURLsRegEx, true)
+	storage := NewHarvestedResourceStorage(contentHarvester, logger, *storageBasePath)
 	twitterAPI := anaconda.NewTwitterApiWithCredentials(*accessToken, *accessSecret, *consumerKey, *consumerSecret)
 
-	file, err := os.OpenFile(*createTestDataFileName, os.O_CREATE|os.O_WRONLY, 0777)
-	defer file.Close()
-	if err != nil {
-		fmt.Printf("Unable to create %s, does its directory exist?\n", *createTestDataFileName)
-		os.Exit(1)
-	}
-	csvWriter := csv.NewWriter(file)
-	csvWriter.Write([]string{"Time", "Tweet", "Original URL", "Finding", "Reason", "Referrer", "Final URL", "Resolved URL", "Cleaned URL"})
-
 	if *searchTwitter {
-		fmt.Printf("Searching Twitter: %s in %s...\n", twitterQuery, *createTestDataFileName)
+		fmt.Printf("Searching Twitter: %s in %s...\n", twitterQuery, *storageBasePath)
 		searchResult, _ := twitterAPI.GetSearch(twitterQuery[0], nil)
 		for _, tweet := range searchResult.Statuses {
-			createTweetTestData(contentHarvester, csvWriter, tweet.Text)
+			//createTweetTestData(contentHarvester, csvWriter, tweet.Text)
+			storage.SaveAllInText(tweet.Text)
 		}
 		return
 	}
 
-	fmt.Printf("Starting Twitter Stream: %s in %s...\n", twitterQuery, *createTestDataFileName)
+	fmt.Printf("Starting Twitter Stream: %s in %s...\n", twitterQuery, *storageBasePath)
 	v := url.Values{"track": twitterQuery}
 	s := twitterAPI.PublicStreamFilter(v)
 
 	for t := range s.C {
 		switch v := t.(type) {
 		case anaconda.Tweet:
-			createTweetTestData(contentHarvester, csvWriter, v.Text)
+			//createTweetTestData(contentHarvester, csvWriter, v.Text)
+			storage.SaveAllInText(v.Text)
 		}
 	}
 }
